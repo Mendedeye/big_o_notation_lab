dab6f903a7cc2fbde68b97dcdabd23ff2e439e0c
Ethan Doering <ethanwdoering@icloud.com>
2023-02-23 10:58:03 -0700

    feat: Added is_under_100 to determine if any of the numbers in a list are 100 or greater.

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,4 @@
-numbers_list = [35,8,64,46,45,54,75,46,35]
+numbers_list = [35,8,64,46,45,54,123,75,46,35]
 
 
 # Task 1: Even or Odd
@@ -15,3 +15,22 @@ number_2 = is_even(8)
 
         # The proper big O notation would be O(1)
 
+# Task 2: Less than 100
+
+# Will return true if all numbers in given_list are under 100 and false if ANY of the numbers are greater than or equal to 100
+        #The proper Big O Notation is going to be O(n)
+def is_under_100(given_list):
+    under_100 = True
+
+    for number in given_list:
+        if number < 100:
+            continue
+        else:
+            under_100 = False
+            break
+
+    return under_100
+
+one_hundred_check = is_under_100(numbers_list)
+
+print(one_hundred_check)
