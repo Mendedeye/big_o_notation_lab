ea95d9142adfbfafe700a58d474f3f626b683809
Ethan Doering <ethanwdoering@icloud.com>
2023-02-23 10:47:41 -0700

    "feat: Added is_even"

---
+++ algorithms.py
@@ -0,0 +1,17 @@
+numbers_list = [35,8,64,46,45,54,75,46,35]
+
+
+# Task 1: Even or Odd
+
+# Will return true if number is even and false if number is odd
+def is_even(number):
+    return bool(number % 2)
+
+number_1 = is_even(9) 
+number_2 = is_even(8)
+
+#print(number_1)
+#print(number_2)
+
+        # The proper big O notation would be O(1)
+
